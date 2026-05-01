9b9613c9616035d8a5acc5abfc504ec19028858b
omzirpe2006 <omzirpe2006@gmail.com>
2026-05-01 17:05:20 +0530

    Code push to self branch

--- if_else_02/pro1.java
+++ if_else_02/pro1.java
@@ -7,9 +7,9 @@ class Pro1{
 	public static void main(String[]args){
 		int x=100;
 		if (x>=1 && x<=1000){
-			System.out.println("Out Rnage");
+			System.out.println("Out Rnage -5");
 		}else {
-			System.out.println("Not In Range");
+			System.out.println("Not In Rangess");
 			
 		}
 	}
